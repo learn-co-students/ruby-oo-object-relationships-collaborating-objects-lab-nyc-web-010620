24f1739b44c119b0fdb6754ab8e16dc98a486413
Filip Todoroski <ftodoroski@ymail.com>
2020-01-13 16:39:11 -0500

    Done.

---
+++ file_method_explanation.md
@@ -0,0 +1,6 @@
+
+# files method
+    ### https://stackoverflow.com/questions/1755665/get-names-of-all-files-from-a-folder-with-ruby
+    ## Get names of all files from a folder with Ruby
+    # Dir["/path/to/search/*"]
+    # The *(asteriks) has a special meaning which is all
---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select { |song| song.artist == self }
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def self.find_or_create_by_name(artist_name)
+        self.all.find { |artist| artist.name == artist_name } ? self.all.find { |artist| artist.name == artist_name } : self.new(artist_name)
+    end
+
+    def print_songs
+        song = Song.all.select { |song| song.artist == self }
+        song.each { |song| puts song.name }
+    end
+end
